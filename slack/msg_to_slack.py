69d3f7ff7b42b0e5bd687b81af3264219389581f
cecilie roras <cecilie.roras@byr.oslo.kommune.no>
2019-12-19 14:09:06 +0100

    message to slack from lambda

--- slack/msg_to_slack.py
+++ slack/msg_to_slack.py
@@ -1,20 +1,19 @@
 import json
 import requests
 
-def handler(event, context):
-    params = event["pathParameters"]
-    slack_data = params["slack"]
+def lambda_handler(event, context):
+    print(event)
+
+    slack_data =  f"{event['message']} - Dato: {event['dato']}"
 
     webhook_url = '***REMOVED***'
-
-    data = json.dumps(slack_data)
+   
     response = requests.post(
-        webhook_url, json={"text": data},
+        webhook_url, json={"text": slack_data},
         headers={'Content-Type': 'application/json'}
     )
 
     if response.status_code != 200:
         raise ValueError(
-            'Request to slack returned an error %s, the response is:\n%s'
-            % (response.status_code, response.text)
+            'Request to slack returned an error %s, the response is:\n%s' % (response.status_code, response.text)
     )
