1e9db09a1bc6631c2c7e9ebaca63543d5e93a804
Håvard Eide <havard.eide@byr.oslo.kommune.no>
2020-02-10 13:47:55 +0100

    DP-42 - following linting standard

--- slack/cloudwatch_error_to_slack.py
+++ slack/cloudwatch_error_to_slack.py
@@ -4,6 +4,7 @@ import requests
 
 SLACK_WEBHOOK_URL = os.environ["SLACK_WEBHOOK_URL"]
 
+
 def lambda_handler(event, context):
     webhook_url = SLACK_WEBHOOK_URL
     for record in event["Records"]:
@@ -12,11 +13,17 @@ def lambda_handler(event, context):
         slack_data = f"*Alarm:* {message['AlarmName']}\n *Reason:* {message['NewStateReason']}\n *At:* {message['StateChangeTime']}\n *Account:* {message['AWSAccountId']}\n"
 
         response = requests.post(
-            webhook_url, json={"text": slack_data, "username": "LambdaError","icon_emoji":":ghost:"},
-            headers={'Content-Type': 'application/json'}
+            webhook_url,
+            json={
+                "text": slack_data,
+                "username": "LambdaError",
+                "icon_emoji": ":ghost:",
+            },
+            headers={"Content-Type": "application/json"},
         )
 
         if response.status_code != 200:
             raise ValueError(
-                'Request to slack returned an error %s, the response is:\n%s' % (response.status_code, response.text)
-        )
+                "Request to slack returned an error %s, the response is:\n%s"
+                % (response.status_code, response.text)
+            )
