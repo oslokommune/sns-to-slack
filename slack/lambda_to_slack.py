bc0ca6b43bb1ccf2a8b891692c241955c1e0d05a
Håvard Eide <havard.eide@byr.oslo.kommune.no>
2020-02-10 13:20:09 +0100

    DP-429 - send cloudwatch errors to slack

--- slack/lambda_to_slack.py
+++ slack/lambda_to_slack.py
@@ -2,20 +2,21 @@ import boto3
 from datetime import datetime
 import json
 
-client = boto3.client('lambda')
+client = boto3.client("lambda")
+
 
 def lambda_handler(event, context):
-    arn = 'arn:aws:lambda:eu-west-1:***REMOVED***:function:lambda-boilerplate-dev-msg_to_slack'
+    arn = "arn:aws:lambda:eu-west-1:***REMOVED***:function:lambda-boilerplate-dev-msg_to_slack"
     data = {
-        'lambdaname': 'CSV-exporter',
-        'status': 'FAILED',
-        'message': 'This message is failing',
-        'dato': datetime.now().isoformat()
-            }
-    
-    response = client.invoke(FunctionName=arn,
-                             InvocationType='RequestResponse',
-                             Payload=json.dumps(data))
+        "lambdaname": "CSV-exporter",
+        "status": "FAILED",
+        "message": "This message is failing",
+        "dato": datetime.now().isoformat(),
+    }
 
-    result = json.loads(response.get('Payload').read())
+    response = client.invoke(
+        FunctionName=arn, InvocationType="RequestResponse", Payload=json.dumps(data)
+    )
+
+    result = json.loads(response.get("Payload").read())
     return result
