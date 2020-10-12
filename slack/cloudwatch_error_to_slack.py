08617b155fa017fda1f21d41da4dac6169fcee74
Jon-Morten Kristiansen <jon.morten.kristiansen@origo.oslo.kommune.no>
2020-10-12 08:22:10 +0200

    DP-1245 Move SNS parsing to functions

--- slack/cloudwatch_error_to_slack.py
+++ slack/cloudwatch_error_to_slack.py
@@ -4,50 +4,60 @@ import requests
 
 WEBHOOK_URL = os.environ["SLACK_ALERTS_WEBHOOK_URL"]
 
-def handler(event, context):
-    for record in event["Records"]:
-        source = record['EventSource']
-        if source != "aws:sns":
-            raise ValueError(
-                "Unsuported 'EventSource' %s. Supported types: 'aws:sns'"
-                % (source)
-            )
 
+def slackTextFromSnsMessage(message):
+    trigger = message["Trigger"]
+    if trigger["Namespace"] != "AWS/Lambda":
+        raise ValueError(
+            "Unsuported SNS message trigger namespace' %s. Supported types: 'AWS/Lambda'"
+            % (trigger["Namespace"])
+        )
+
+    try:
+        # Assumed format: "arn:aws:cloudwatch:eu-west-1:***REMOVED***:alarm:ALARM_NAME"
+        region = message["AlarmArn"].split(":")[3]
+    except (KeyError, IndexError):
+        region = "eu-west-1"
+
+    function_name = None
+    for dim in trigger["Dimensions"]:
+        if dim["name"] == "FunctionName":
+            function_name = dim["value"]
+            break
+
+    if function_name is None:
+        raise ValueError("Lambda function name not found.")
+
+    reason = message["NewStateReason"]
+    monitor_url = f"https://{region}.console.aws.amazon.com/lambda/home?region={region}#/functions/{function_name}?tab=monitoring"
+
+    return (
+        f"Lambda function *{function_name}* failed.\n"
+        f"Reason: {reason}\n"
+        f"<{monitor_url}|Monitoring>\n"
+    )
+
+
+def slackTextFromRecord(record):
+    source = record["EventSource"]
+    if source == "aws:sns":
         sns = record["Sns"]
         message = json.loads(sns["Message"])
-
-        try:
-            # Assumed format: "arn:aws:cloudwatch:eu-west-1:***REMOVED***:alarm:ALARM_NAME"
-            region = message["AlarmArn"].split(":")[3]
-        except IndexError:
-            region = "eu-west-1"
-
-        prefix = "Dataplatform_LambdaError_"
-        function_name = (
-            message["AlarmName"].split(prefix, 1)
-            if prefix in message["AlarmName"]
-            else message["AlarmName"]
+        return slackTextFromSnsMessage(message)
+    else:
+        raise ValueError(
+            "Unsuported 'EventSource' %s. Supported types: 'aws:sns'" % (source)
         )
 
-        trigger = message["Trigger"]
-        if trigger["Namespace"] == "AWS/Lambda":
-            for dim in trigger["Dimensions"]:
-                if dim["name"] == "FunctionName":
-                    function_name = dim["value"]
-                    break
 
-        monitor_url = f"https://{region}.console.aws.amazon.com/lambda/home?region={region}#/functions/{function_name}?tab=monitoring"
-
-        slack_data = (
-            f"Lambda function *{function_name}* failed.\n"
-            f"Reason: {message['NewStateReason']}\n"
-            f"<{monitor_url}|Monitoring>\n"
-        )
+def handler(event, context):
+    for record in event["Records"]:
+        slackText = slackTextFromRecord(record)
 
         response = requests.post(
             WEBHOOK_URL,
             json={
-                "text": slack_data,
+                "text": slackText,
             },
             headers={"Content-Type": "application/json"},
         )
